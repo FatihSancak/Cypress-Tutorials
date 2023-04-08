165f217e4e2c8a6e9ea8554ead7d61e355bd7f66
Fatih SANCAK <fsancak@gmail.com>
2023-04-08 23:55:01 +0300

    Scroll Controls

---
+++ cypress/e2e/com.sancak/day03/day03_scroll.cy.js
@@ -0,0 +1,26 @@
+describe('Scroll TEsti',()=>{
+    it('Scrool into view',()=>{
+
+        cy.visit('https://qa-environment.koalaresorthotels.com/')
+        cy.wait(3000)
+
+        cy.get('body > :nth-child(6)').scrollIntoView()
+
+        cy.wait(3000)
+        cy.get(':nth-child(9) > .container > .justify-content-center').scrollIntoView()
+    
+        cy.wait(2000)
+        cy.get('.container-fluid > .pb-5').scrollIntoView()
+
+
+        cy.scrollTo('bottom')
+        cy.wait(2000)
+
+        cy.scrollTo('top')
+
+        cy.scrollTo(0,1000) // (X,Y) koordinatlarına göre sayfa hareket edecektir. 
+
+        cy.scrollTo(0,-900)
+    })
+    
+})
