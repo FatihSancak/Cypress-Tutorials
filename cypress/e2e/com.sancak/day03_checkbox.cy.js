6a941563139a97f2b081e6b2baa08a10da436f9f
Fatih SANCAK <fsancak@gmail.com>
2023-04-08 23:53:49 +0300

    CheckBox Controls

--- cypress/e2e/com.sancak/day03_checkbox.cy.js
+++ cypress/e2e/com.sancak/day03_checkbox.cy.js
@@ -1,5 +1,5 @@
 describe('Checkbox', () => {
-    it('Checkbox Test', () => {
+    it('Checking Single Box', () => {
 
         cy.visit('http://automationpractice.pl/')
         cy.get('.sf-menu > :nth-child(1)').click()
@@ -16,10 +16,18 @@ describe('Checkbox', () => {
         cy.wait(3000)
         // Uncheck 
         cy.get('#layered_category_4').uncheck()
-        
+
         cy.get('#layered_category_4').should('not.be.checked')
+    })
 
+    it.only('Checking All Checkboxes',() => {
 
+        cy.visit('http://automationpractice.pl/')
+        cy.get('.sf-menu > :nth-child(1)').click()
+        // Tüm input türü CHECKBOX olanların hepsini CHECK eder.
+        cy.get("input[type=checkbox").check()
+        cy.wait(2*1000)
+        cy.get("input[type=checkbox").uncheck()
 
     })
 })
